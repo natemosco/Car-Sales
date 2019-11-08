6dde8567299c68a8aa17cc91440f266f2663dcf1
Nathaniel Mosco <natemosco@gmail.com>
2019-11-07 21:05:57 -0500

    spread initial store to respective components. still needs actions to be passed down

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from "react-redux";
 
 import AddedFeature from './AddedFeature';
 
@@ -13,10 +14,14 @@ const AddedFeatures = props => {
           ))}
         </ol>
       ) : (
-        <p>You can purchase items from the store.</p>
-      )}
+          <p>You can purchase items from the store.</p>
+        )}
     </div>
   );
 };
-
-export default AddedFeatures;
+const mapPropsToValues = state => {
+  return {
+    car: state.car
+  }
+}
+export default connect(mapPropsToValues, {})(AddedFeatures);
