79a610bdfd6214636b42d89a471141ab0b1a11b7
Nathaniel Mosco <natemosco@gmail.com>
2019-11-08 18:45:57 -0500

    added functions to appropriate components
    edited switch case to return appropriate non mutated code

---
+++ src/reducers/index.js
@@ -0,0 +1,56 @@
+import { createStore } from "redux";
+import { REMOVE_FEATURE, BUY_ITEM } from "../actions"
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+        price: 26395,
+        name: '2019 Ford Mustang',
+        image:
+            'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+        features: []
+    },
+    additionalFeatures: [
+        { id: 1, name: 'V-6 engine', price: 1500 },
+        { id: 2, name: 'Racing detail package', price: 1500 },
+        { id: 3, name: 'Premium sound system', price: 500 },
+        { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case "BUY_ITEM":
+            if (state.car.features.filter(obj => obj.id === action.payload.id).length === 0) {
+                return {
+                    ...state,
+                    car: {
+                        ...state.car,
+                        features: [...state.car.features, action.payload]
+                    }
+                }
+            } else {
+                return state;
+            }
+
+        case "REMOVE_FEATURE":
+            return {
+                ...state,
+                car: {
+                    ...state.car,
+                    features: state.car.features.filter(obj => (obj.id !== action.payload.id))
+                }
+            };
+        default:
+            return state;
+    }
+
+}
+
+
+const store = createStore(reducer);
+
+export { reducer, store }
+
+
+
